591e82af694e2074caa2e1e5e9831d538f8a26ed
jakegodsall <jake.edward.godsall@gmail.com>
2025-02-23 12:26:25 +0000

    added crud methods for config

--- src/config/config.go
+++ src/config/config.go
@@ -12,7 +12,7 @@ type Config struct {
 	DatabaseID     string
 }
 
-func CreateConfigDir() (string, error) {
+func GetOrCreateConfigDir() (string, error) {
 	home, err := os.UserHomeDir()
 	if err != nil {
 		return "", fmt.Errorf("could not access the user's home directory: %v", err)
@@ -27,4 +27,50 @@ func CreateConfigDir() (string, error) {
 	}
 
 	return config, nil
+}
+
+func ConfigFileExists(filename string) (bool, error) {
+	configDir, err := GetOrCreateConfigDir()
+	if err != nil {
+		return false, err
+	}
+	configPath := filepath.Join(configDir, filename)
+	_, err = os.Stat(configPath)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func WriteConfig(filename string, data []byte) error {
+	configDir, err := GetOrCreateConfigDir()
+	if err != nil {
+		return err
+	}
+
+	configPath := filepath.Join(configDir, filename)
+	return os.WriteFile(configPath, data, 0644)
+}
+
+func ReadConfig(filename string) ([]byte, error) {
+	configDir, err := GetOrCreateConfigDir()
+	if err != nil {
+		return nil, err
+	}
+
+	configPath := filepath.Join(configDir, filename)
+	return os.ReadFile(configPath)
+}
+
+func DeleteConfig(filename string) error {
+	configDir, err := GetOrCreateConfigDir()
+	if err != nil {
+		return err
+	}
+
+	configPath := filepath.Join(configDir, filename)
+	return os.Remove(configPath)
 }
