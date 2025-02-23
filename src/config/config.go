de3ab18b5f093fe1fe44e183728ebacb4e72572e
jakegodsall <jake.edward.godsall@gmail.com>
2025-02-23 12:31:03 +0000

    updated to use json

--- src/config/config.go
+++ src/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -8,8 +9,8 @@ import (
 )
 
 type Config struct {
-	NotionApiToken string
-	DatabaseID     string
+	NotionApiToken string `json:"notion_api_token"`
+	DatabaseID     string `json:"database_id"`
 }
 
 func GetOrCreateConfigDir() (string, error) {
@@ -45,24 +46,39 @@ func ConfigFileExists(filename string) (bool, error) {
 	return false, err
 }
 
-func WriteConfig(filename string, data []byte) error {
-	configDir, err := GetOrCreateConfigDir()
-	if err != nil {
-		return err
-	}
-
-	configPath := filepath.Join(configDir, filename)
-	return os.WriteFile(configPath, data, 0644)
-}
-
-func ReadConfig(filename string) ([]byte, error) {
+func ReadConfig(filename string) (Config, error) {
+	var config Config
 	configDir, err := GetOrCreateConfigDir()
 	if err != nil {
 		return nil, err
 	}
 
 	configPath := filepath.Join(configDir, filename)
-	return os.ReadFile(configPath)
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return config, err
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return config, fmt.Errorf("could not unmarshal JSON config: %v", err)
+	}
+
+	return config, nil
+}
+
+func WriteConfig(filename string, config Config) error {
+	configDir, err := GetOrCreateConfigDir()
+	if err != nil {
+		return err
+	}
+
+	configPath := filepath.Join(configDir, filename)
+	data, err := json.MarshalIndent(config, "", " ")
+	if err != nil {
+		return fmt.Errorf("could not marshal config to JSON: %v", err)
+	}
+	return os.WriteFile(configPath, data, 0644)
 }
 
 func DeleteConfig(filename string) error {
